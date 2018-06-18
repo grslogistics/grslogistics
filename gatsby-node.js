0c91d48becd82b4887200da9724d6de538641920
Alex Kozlov <mediumespresso@gmail.com>
2018-06-18 14:25:26 +0300

    cms fixes

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.onCreateNode = function ({ node }) {
-  if (node.internal.type === 'File') {
-    console.log(node)
-  }
-}
-
-// exports.onCreatePage = function ({ page }) {
-//   console.log('#PAGE', page, '###')
-// }
